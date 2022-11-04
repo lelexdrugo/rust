6f72de93e5567b6e17761783a073fc0090594f68
GABRIELE SCAFFIDI MILITONE s288200 <s288200@studenti.polito.it>
2022-11-04 17:03:55 +0100

    feat: some initial exercises

--- exercises/practice/clock/src/lib.rs
+++ exercises/practice/clock/src/lib.rs
@@ -1,15 +1,34 @@
-pub struct Clock;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug, PartialEq)]
+pub struct Clock{
+    hours: i32,
+    minutes: i32
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let mut total_min = (hours*60 + minutes)%1440;
+        if total_min<0{
+            total_min = 1440 + total_min
+        }
+        Self{hours: total_min/60, minutes: total_min%60}
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+
+
+        Clock::new(self.hours, self.minutes+minutes)
+    }
+/*
+    pub fn to_string(&self) -> String {
+        format!("{:02}:{:02}", self.hours, self.minutes)
+    }
+*/
+}
+
+impl Display for Clock{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
--- exercises/practice/reverse-string/src/lib.rs
+++ exercises/practice/reverse-string/src/lib.rs
@@ -1,3 +1,29 @@
+#[cfg(feature = "grapheme")]
+use unicode_segmentation::UnicodeSegmentation;
+
+#[cfg(not(feature = "grapheme"))]
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    let mut new_string = String::new();
+    for var in input.chars().rev(){
+        new_string.push(var);
+    }
+
+    //return input.chars().rev().collect();
+
+    println!("Original string: {}, reversed string: {}", input, new_string);
+    return new_string;
 }
+#[cfg(feature = "grapheme")]
+pub fn reverse(input: &str) -> String {
+    let mut new_string = String::new();
+
+    for var in input.graphemes(true).rev(){
+        new_string.push_str(var);
+    }
+
+    //return input.chars().rev().collect();
+
+    println!("Original string: {}, reversed string: {}", input, new_string);
+    return new_string;
+}
+
