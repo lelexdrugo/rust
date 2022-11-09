e0651b78c4a9502ed486dbed6dc32a4ab5f6cea3
lelexdrugo <gabrielescaffidi@gmail.com>
2022-11-09 16:37:53 +0100

    feat: exercise with structure, destructure and option

--- exercises/concept/role-playing-game/src/lib.rs
+++ exercises/concept/role-playing-game/src/lib.rs
@@ -2,6 +2,8 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+use std::cmp::min;
+
 pub struct Player {
     pub health: u32,
     pub mana: Option<u32>,
@@ -10,10 +12,28 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        match self.health {
+            0 => Option::from( match self.level {
+                0..=9 => Player { health: 100, mana: Option::None, level: self.level },
+                10.. => Player { health: 100, mana: Option::Some(100), level: self.level },
+            }),
+            _ => Option::None,//Player { ..*self },
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        return if(self.mana.is_none()){
+            self.health -= min(mana_cost, self.health);
+            0
+        }
+        else if self.mana.is_some() && self.mana.unwrap()<=mana_cost {
+            0
+        } else {
+            let mut mana = self.mana.unwrap();
+            mana -= mana_cost;
+            self.mana = Option::Some(mana);
+            2 * mana_cost
+        }
     }
 }
+
